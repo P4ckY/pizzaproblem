7d90141cef13b595469ce971f551b53d36f5bc8c
Flashnos <adrian.goerisch2013@gmail.com>
2017-02-22 22:08:36 +0100

    Updated Java

--- DataSetReader.java
+++ DataSetReader.java
@@ -6,13 +6,21 @@ import java.io.IOException;
 public class DataSetReader {
 
 	private int[] ar = new int[4];
-	private int[][] pizza;
+	private int[][] pizza; // Reihen / Spalten
 	private File f;
-	
+
+	/**
+	 * Constructor
+	 * @param name Name of dataset file (without filetype)
+	 */
 	public DataSetReader(String name){
 		setFilename(name);
 	}
-	
+
+	/**
+	 * sets file to read from
+	 * @param name filename
+	 */
 	public void setFilename(String name){
 		f = new File(name+".in");
 		if(f.exists()){
@@ -21,73 +29,85 @@ public class DataSetReader {
 			System.out.println("File \""+name+".in\" does not exist");
 		}
 	}
-	
+
+	/**
+	 * reads entire file and writes all information in two arrays
+	 * @param f File
+	 */
 	public void readfile(File f){
-		
+
 		if(f != null){
 			BufferedReader br;
-			
+
 			try {
-				
+
 				br = new BufferedReader(new FileReader(f.getName()));
-				
+
 				int r = br.read();
 				String number = "";
-				
+
 				for(int i = 0; i < 4 && r != -1; i++){
-					
+
 					if(r == 32){ // Leerzeichen am Anfang
 						r = br.read();
 					}
-					
+
 					while( r != 32 && r != 10){ // Leerzeichen oder Zeilenumbruch beenden Zahl
 						number += (char)r;
 						r = br.read();
 					}
-					
-					if(!number.equals("")){ 
+
+					if(!number.equals("")){
 						ar[i] = Integer.parseInt(number);
 						number = "";
 					}
-					
+
 				}
-							
+
 				// erstes Array eingelesen
-				
+
 				pizza = new int[ar[0]][ar[1]];
-				
+
 				for(int i = 0; i < ar[0]; i++){
 					for(int j = 0; j < ar[1]; j++){
 						r = br.read();
 						while(r != 77 && r != 84){
 							r = br.read();
 						}
-						
+
 						if(r == 84){
 							pizza[i][j] = 2;
 						}else if(r == 77){
 							pizza[i][j] = 1;
 						}
-					}	
+					}
 				}
-				
+
 				// zweites Array eingelesen
-				
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}else{
 			System.out.println("Please specify filename");
 		}
-		
+
 	}
-	
+
+	/**
+	 * getter
+	 * @return 2D array pizza
+	 */
 	public int[][] getPizza(){
 		return pizza;
 	}
-	
+
+	/**
+	 * getter
+	 * @return 1D array information
+	 */
 	public int[] getArray(){
 		return ar;
 	}
-		
+
 }
--- Starter.java
+++ Starter.java
@@ -3,6 +3,6 @@ public class Starter {
 	public static void main(String[] args){
 		@SuppressWarnings("unused")
 		DataSetReader ifr = new DataSetReader("small");
-	}	
-	
+	}
+
 }
